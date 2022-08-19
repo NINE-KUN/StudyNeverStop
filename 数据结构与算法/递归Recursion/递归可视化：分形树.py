f21ee264a12012fee08b4abe96a6442649aec326
qiankun <12345678>
2022-08-19 17:05:27 +0800

    递归

--- 数据结构与算法/递归Recursion/递归可视化：分形树.py
+++ 数据结构与算法/递归Recursion/递归可视化：分形树.py
@@ -11,12 +11,30 @@
 import turtle
 
 t=turtle.Turtle()
-def drawSpiral(t,lineLen):
-    if lineLen > 0:
-        t.forward(lineLen)
-        t.right(90)
-        drawSpiral(t,lineLen-5)
-drawSpiral(t,100)
-turtle.done()
+# def drawSpiral(t,lineLen):
+#     if lineLen > 0:
+#         t.forward(lineLen)
+#         t.right(90)
+#         drawSpiral(t,lineLen-5)
+# drawSpiral(t,100)
+# turtle.done()
 '''分形Fractal 
     一个粗糙或者零碎的几何形状，可以分成数个部分，且每一部分都(至少近似地)是整体缩小后的形状，即具有自相似的性质'''
+def tree(branch_len):
+    if branch_len>5:
+        t.forward(branch_len)
+        t.right(20)
+        tree(branch_len-15)
+        t.left(40)
+        tree(branch_len-15)
+        t.right(20)
+        t.backward(branch_len)
+t.left(90)
+t.penup()
+t.backward(100)
+t.pendown()
+t.pencolor('black')
+t.pensize(2)
+tree(75)
+t.hideturtle()
+turtle.done()
