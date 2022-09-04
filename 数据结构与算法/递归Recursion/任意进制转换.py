0c786294a0c14fa695db80219bf1193570f770c2
qiankun <12345678>
2022-09-04 11:49:16 +0800

    递归

--- 数据结构与算法/递归Recursion/任意进制转换.py
+++ 数据结构与算法/递归Recursion/任意进制转换.py
@@ -4,5 +4,6 @@ def toStr(n,base):
     if n<base:
         return convertString[n] #最小规模
     else:
+        '''除以进制基base(//base) 对进制基求余数(%base  )'''
         return toStr(n//base,base)+convertString[n%base] #减小规模调用自身
 print(toStr(1453,16))
