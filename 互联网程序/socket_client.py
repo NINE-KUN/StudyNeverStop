a24a24c24912c0f6b1517bedf77506abc29ee323
qiankun <12345678>
2022-09-20 18:00:05 +0800

    socket

---
+++ 互联网程序/socket_client.py
@@ -0,0 +1,47 @@
+'''客户端
+接下来我们写一个简单的客户端实例连接到以上创建的服务。端口号为 9999。
+socket.connect(hostname, port ) 方法打开一个 TCP 连接到主机为 hostname 端口为 port 的服务商。
+连接后我们就可以从服务端获取数据，记住，操作完成后需要关闭连接。
+完整代码如下：'''
+
+'''         socket编程
+        Server(服务器端)                    Client(客户端)
+        Socket                                      
+          |
+        bind(协议、地址、端口)
+          |
+        listen(监听客户端socket请求)
+          |
+        accept()                           Socket
+          |                                   |
+    ———>阻塞等待连接请求(新套接字) <---三次握手--connect()
+   |      |                                   |
+   |    recv()<----------------------------send()
+   |      |                                   |
+    ————send()---------------------------->recv()
+          |                                   |
+        close()<---------------------------close()
+'''
+
+# 导入socket、sys模块
+import socket
+import sys
+
+# 创建socket对象
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# 获取本地主机名
+host = socket.gethostname()
+
+# 设置端口号
+port = 9999
+
+# 链接服务、指定主机和端口
+s.connect((host, port))
+
+# 接收服务器端发送的数据 小于1024字节的数据
+msg = s.recv(1024) #bat类型 需要转码为utf8
+
+s.close()
+
+print(msg.decode('utf-8'))
